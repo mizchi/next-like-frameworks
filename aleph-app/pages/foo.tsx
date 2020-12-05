f7e128da085719b2fc27fe2a094def8c7dc6efab
mizchi <miz404@gmail.com>
2020-12-05 19:16:51 +0900

    Add aleph

---
+++ aleph-app/pages/foo.tsx
@@ -0,0 +1,6 @@
+import { Import, useDeno } from "https://deno.land/x/aleph/mod.ts";
+import React, { useState } from "https://esm.sh/react";
+
+export default function Foo() {
+  return <div className="page">foo</div>;
+}
